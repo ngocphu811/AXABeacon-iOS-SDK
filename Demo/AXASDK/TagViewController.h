71253991a8e06d1866335573581c117ff17a9340
AXAET <rd02@axaet.net>
2015-07-17 12:37:29 +0800

    first commit

---
+++ Demo/AXASDK/TagViewController.h
@@ -0,0 +1,14 @@
+//
+//  TagViewController.h
+//  AXASDK
+//
+//  Created by AXAET_APPLE on 15/7/15.
+//  Copyright (c) 2015年 axaet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SDK/SDK.h>
+
+@interface TagViewController : UITableViewController
+
+@end
---
+++ SDK.framework/Headers/SDK.h
@@ -0,0 +1,22 @@
+//
+//  SDK.h
+//  SDK
+//
+//  Created by AXAET_APPLE on 15/7/17.
+//  Copyright (c) 2015年 axaet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDK.
+FOUNDATION_EXPORT double SDKVersionNumber;
+
+//! Project version string for SDK.
+FOUNDATION_EXPORT const unsigned char SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDK/PublicHeader.h>
+
+#import <SDK/AXABeacon.h>
+#import <SDK/AXABeaconManager.h>
+
+
